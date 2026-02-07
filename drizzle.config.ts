a5af039ce9e8648657cd3c22f546d030109f3747
roshanpatil2000 <roshanpatil2912.rp@gmail.com>
2026-02-07 22:36:28 +0530

    make minor twicks

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'drizzle-kit';
 
-export default defineConfig({
-    out: './drizzle',
-    schema: './db/schema.ts',
-    dialect: 'postgresql',
-    dbCredentials: {
-        url: process.env.DATABASE_URL!,
-    },
-});
+export default defineConfig(
+    {
+        out: './drizzle',
+        schema: './db/schema.ts',
+        dialect: 'postgresql',
+        dbCredentials: {
+            url: process.env.DATABASE_URL!,
+        },
+    }
+);
